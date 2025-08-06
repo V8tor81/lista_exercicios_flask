0dd29a779c1a6ee0402c6b7ded9d5ec1d43b7d84
Maria Vitória <132756495+V8tor81@users.noreply.github.com>
2025-08-06 05:56:17 -0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, flash, request
 from app import app, db
-from app.forms.medico_form import medicoform
+from app.forms.medico_form import MedicoForm
+from app.controllers.MedicoController import salvar_medico
+from app.controllers.MedicoController import listar_medicos
+from app.controllers.MedicoController import buscar_medico_por_id
+from app.controllers.MedicoController import buscar_medico_por_id, atualizar_medico
+from app.controllers.MedicoController import buscar_medico_por_id, excluir_medico
 
 
 @app.route('/')
@@ -8,9 +13,61 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/medicos', methods=['GET’, ‘POST'])
+@app.route('/medicos/novo', methods=['GET’, ‘POST'])
 def novo():
-    formulario = medicoform()
-    return render_template('medico_form.html', title='medicoform',form=formulario)
+    form = MedicoForm()
+    if form.validate_on_submit():
+        salvar_medico(form)
+        flash("Médico Cadastrado Com Sucesso")
+        return redirect(url_for('listar_medicos'))
+    return render_template('medico_form.html',form=form)
+
+@app.route('/medicos')
+def listar_medicos():
+    medicos=  listar_medicos
+    return render_template('medico_lista.html', medicos=medicos)
+
+
+@app.route('/medicos/<int:id>')
+def detalhes_medico(id):
+    medico = buscar_medico_por_id(id)
+    if not medico:
+        flash ("Esse médico não foi encontrado")
+        return redirect(url_for('listar_medicos'))
+    render_template('medico_detalhes.html',medico=medico)
+
+
+
+@app.route('/medicos/<int:id>/editar', methods=['GET', 'POST'])
+def editar_medico(id):
+    medico = buscar_medico_por_id(id)
+    if not medico:
+        flash('Médico não encontrado.', 'danger')
+        return redirect(url_for('listar_medicos'))
+
+    form = MedicoForm(obj=medico)  # Carrega os dados no formulário
+
+    if form.validate_on_submit():
+        atualizar_medico(medico, form)
+        flash('Médico atualizado com sucesso!', 'success')
+        return redirect(url_for('detalhes_medico', id=medico.id))
+
+    return render_template('medico_form.html', form=form)
+
+
+@app.route('/medicos/<int:id>/excluir', methods=['GET', 'POST'])
+def excluir_medico_view(id):
+    medico = buscar_medico_por_id(id)
+    if not medico:
+        flash('Médico não encontrado.')
+        return redirect(url_for('listar_medicos'))
+
+    if request.method == 'POST':
+        excluir_medico(medico)
+        flash('Médico excluído com sucesso.')
+        return redirect(url_for('listar_medicos'))
+
+    return render_template('medico_excluir.html', medico=medico)
+
 
 
