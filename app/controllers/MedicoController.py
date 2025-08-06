1931b9b8183410a2fd410536355bfd7705c7d9e2
Maria Vitória <132756495+V8tor81@users.noreply.github.com>
2025-08-06 05:49:49 -0300

    Update MedicoController.py

---
+++ app/controllers/MedicoController.py
@@ -0,0 +1,39 @@
+from app import db
+from app import flash
+from app.models.medicos import Medico
+
+
+def salvar_medico(form):
+    novo_medico = Medico(
+        nome_completo=form.nome_completo.data,
+        username=form.username.data,
+        crm=form.crm.data,
+        especialidade=form.especialidade.data,
+        email=form.email.data
+    )
+    db.session.add(novo_medico)
+    db.session.commit()
+    flash("Médico cadastrado com sucesso!", "success")
+
+
+def listar_medicos():
+    return Medico.query.all()
+
+def buscar_medico_por_id(id):
+    return Medico.query.get(id)
+
+def atualizar_medico(medico, form):
+    medico.nome_completo = form.nome_completo.data
+    medico.username = form.username.data
+    medico.crm = form.crm.data
+    medico.especialidade = form.especialidade.data
+    medico.email = form.email.data
+    db.session.commit()
+    flash("Médico atualizado com sucesso!", "success")
+
+def excluir_medico(medico):
+    db.session.delete(medico)
+    db.session.commit()
+    flash("Médico excluído com sucesso!", "success")
+
+
